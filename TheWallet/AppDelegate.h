db6fbfd447570cd18b4a89c2632807172ee5044e
AOA <akixe.otegi@sgsmap.com>
2016-08-14 11:51:47 +0200

    Initial Commit

---
+++ TheWallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheWallet
+//
+//  Created by Akixe on 14/8/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheWallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheWallet
+//
+//  Created by Akixe on 14/8/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
