7d179fe6ba103745db293d084317653c248d1718
AOA <akixe.otegi@sgsmap.com>
2016-08-19 18:27:32 +0200

    40: Implementar protocolo en Wallet

--- TheWallet/Wallet.m
+++ TheWallet/Wallet.m
@@ -8,6 +8,37 @@
 
 #import "Wallet.h"
 
+@interface Wallet()
+@property (nonatomic, strong) NSMutableArray *moneys;
+
+@end
 @implementation Wallet
 
+- (id) initWithAmount:(NSInteger)amount currency:(NSString *)currency
+{
+    if (self = [super init]) {
+        Money *money = [[Money alloc] initWithAmount:amount currency:currency];
+        _moneys = [NSMutableArray array];
+        [_moneys addObject:money];
+    }
+    return self;
+}
+
+- (id<Money>) plus:(Money *)money
+{
+    [self.moneys addObject:money];
+    return self;
+}
+
+- (id<Money>) times:(NSInteger)multiplier
+{
+    NSMutableArray *newMoneys = [NSMutableArray arrayWithCapacity:self.moneys.count];
+    
+    for (Money *money in self.moneys) {
+        Money *newMoney = [money times:multiplier];
+        [newMoneys addObject:newMoney];
+    }
+    self.moneys = newMoneys;
+    return self;
+}
 @end
